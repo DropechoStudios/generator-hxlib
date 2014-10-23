4010555ab2ee8e9dc2e2558581f835e3e17521fa
Ben Van Treese <vantreeseba@gmail.com>
2014-10-22 20:07:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# generator-haxepunk [![Build Status](https://secure.travis-ci.org/DropechoStudios/generator-haxepunk.png?branch=master)](https://travis-ci.org/DropechoStudios/generator-haxepunk)
+
+## License
+
+MIT
---
+++ app/index.js
@@ -0,0 +1,70 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var path = require('path');
+
+module.exports = yeoman.generators.Base.extend({
+    init: function init() {
+        this.on('end', function() {
+            this.installDependencies({
+                bower: false
+            });
+        });
+    },
+
+    askFor: function askFor() {
+        var _self = this,
+            done = _self.async();
+
+        var prompts = [{
+            type: 'input',
+            name: 'username',
+            message: 'What is your haxelib/github username?'
+        }, {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your library?'
+        }];
+
+        _self.prompt(prompts, function(props) {
+            _self._.each(prompts, function(prompt) {
+                _self[prompt.name] = props[prompt.name];
+            });
+
+            done();
+        });
+    },
+
+    config: function config() {
+        this.libClass = this._.classify(this.name);
+    },
+
+    src: function src() {
+        var name = this.name;
+
+        this._templateDirectory('base', '.');
+        this.mkdir('src');
+        this.mkdir('src/' + name);
+        this.template('src/_lib.hx', 'src/' + name + '/' + this.libClass + '.hx');
+    },
+
+    _templateDirectory: function _templateDirectory(source, destination) {
+        destination = destination || source;
+        var root = this.isPathAbsolute(source) ? source : path.join(this.sourceRoot(), source);
+        var files = this.expandFiles('**', {
+            dot: true,
+            cwd: root
+        });
+
+        for (var i = 0; i < files.length; i++) {
+            var f = files[i];
+            var src = path.join(root, f);
+            if (path.basename(f).indexOf('_') == 0) {
+                var dest = path.join(destination, path.dirname(f), path.basename(f).replace(/^_/, ''));
+                this.template(src, dest);
+            } else {
+                var dest = path.join(destination, f);
+                this.copy(src, dest);
+            }
+        }
+    }
+});
